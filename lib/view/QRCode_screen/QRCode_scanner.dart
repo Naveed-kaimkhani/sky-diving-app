744f98774ca6205adc365accd3af032b513edc38
naveed-kaimkhani <naveedkaimkhami@gmail.com>
2025-05-06 05:57:15 +0500

    .

--- lib/view/QRCode_screen/QRCode_scanner.dart
+++ lib/view/QRCode_screen/QRCode_scanner.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-// import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
 import 'package:sky_diving/view_model/auth_controller.dart';
 
 class QRCodeScanner extends StatelessWidget {
@@ -13,22 +13,22 @@ class QRCodeScanner extends StatelessWidget {
       appBar: AppBar(title: Text('Scan Referral Code')),
       body: Column(
         children: [
-          // Expanded(
-          //   flex: 5,
-          //   child: QRView(
-          //     key: qrKey,
-          //     onQRViewCreated: (QRViewController controller) {
-          //       controller.scannedDataStream.listen((scanData) {
-          //         if (scanData.code != null &&
-          //             authController.referralCode.value.isEmpty) {
-          //           authController.referralCode.value = scanData.code!;
-          //           controller.pauseCamera();
-          //           Get.back(); // close scanner screen
-          //         }
-          //       });
-          //     },
-          //   ),
-          // ),
+          Expanded(
+            flex: 5,
+            child: QRView(
+              key: qrKey,
+              onQRViewCreated: (QRViewController controller) {
+                controller.scannedDataStream.listen((scanData) {
+                  if (scanData.code != null &&
+                      authController.referralCode.value.isEmpty) {
+                    authController.referralCode.value = scanData.code!;
+                    controller.pauseCamera();
+                    Get.back(); // close scanner screen
+                  }
+                });
+              },
+            ),
+          ),
           Expanded(
             flex: 1,
             child: Obx(() => Center(
