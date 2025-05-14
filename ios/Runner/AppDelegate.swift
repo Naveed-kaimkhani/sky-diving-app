95375a9e18639592bbb730558e064f2e866460b9
naveed_kk <naveedkaimkhami@gmail.com>
2025-05-14 06:10:52 +0500

    .

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -6,22 +6,26 @@ import flutter_branch_sdk
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
-    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Correct: Use instance of FlutterBranchSdkPlugin
-    FlutterBranchSdkPlugin.sharedInstance().application(application, didFinishLaunchingWithOptions: launchOptions ?? [:])
-
+    // Initialize Branch SDK
+    FlutterBranchSdkPlugin.application(application, didFinishLaunchingWithOptions: launchOptions)
+    
+    // Register Flutter plugins
     GeneratedPluginRegistrant.register(with: self)
+    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
+  // Handle deep links
   override func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-    FlutterBranchSdkPlugin.sharedInstance().application(application, open: url, options: options)
+    FlutterBranchSdkPlugin.application(application, open: url, options: options)
     return super.application(application, open: url, options: options)
   }
 
+  // Handle Universal Links
   override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-    FlutterBranchSdkPlugin.sharedInstance().application(application, continue: userActivity, restorationHandler: restorationHandler as! ([Any]) -> Void)
+    FlutterBranchSdkPlugin.application(application, continue: userActivity, restorationHandler: restorationHandler)
     return super.application(application, continue: userActivity, restorationHandler: restorationHandler)
   }
-}
+} 
