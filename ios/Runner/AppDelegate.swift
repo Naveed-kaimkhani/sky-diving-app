a2e6f71533435d1440c299ed1a86dec1d164997a
naveed_kk <naveedkaimkhami@gmail.com>
2025-05-14 06:58:29 +0500

    .

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,31 +1,13 @@
-import UIKit
 import Flutter
+import UIKit
 import flutter_branch_sdk
-
 @main
 @objc class AppDelegate: FlutterAppDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    // Initialize Branch SDK
-    FlutterBranchSdkPlugin.application(application, didFinishLaunchingWithOptions: launchOptions)
-    
-    // Register Flutter plugins
     GeneratedPluginRegistrant.register(with: self)
-    
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
-
-  // Handle deep links
-  override func application(_ application: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-    FlutterBranchSdkPlugin.application(application, open: url, options: options)
-    return super.application(application, open: url, options: options)
-  }
-
-  // Handle Universal Links
-  override func application(_ application: UIApplication, continue userActivity: NSUserActivity, restorationHandler: @escaping ([UIUserActivityRestoring]?) -> Void) -> Bool {
-    FlutterBranchSdkPlugin.application(application, continue: userActivity, restorationHandler: restorationHandler)
-    return super.application(application, continue: userActivity, restorationHandler: restorationHandler)
-  }
-} 
+}
